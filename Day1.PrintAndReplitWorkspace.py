a3532fc3c3f88d934367c1dfceba82c8dec59f54
mashfiqur11 <mashfiqur07@gmail.com>
2024-03-26 00:06:14 +0600

    adding day1's code

---
+++ Day1.PrintAndReplitWorkspace.py
@@ -0,0 +1,43 @@
+
+print ("My answer")     ### Everything in the quotes is text is called a string.
+print ("How are you?")  ### Automatically will be printed in new line.
+print ("😎")
+
+
+### The three quotes of printing printing just copies the text exactly as it is appears.
+print ("""
+Here's an extract 
+       Bit 
+        of 
+       Text
+""")
+
+
+### Challenge for Day 1
+
+"""
+Mark
+September 20th 2029
+I am signing up for Replit's 100 days of Python
+challenge!
+I will make sure to spend some time every day coding
+along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do
+this from my phone wherever I happen to be. No excuses
+for not coding from the middle of a field!
+
+"""
+### print it in one single print command and in next line print I am feeling 🥳
+
+
+print ("""
+Mark
+September 20th 2029
+I am signing up for Replit's 100 days of Python
+challenge!
+I will make sure to spend some time every day coding
+along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do
+this from my phone wherever I happen to be. No excuses
+for not coding from the middle of a field!""")
+print ("I am feeling 🥳")
