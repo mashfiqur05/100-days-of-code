b15e203b74bd668925e25dbdddac9fc0d0f959a3
mashfiqur11 <mashfiqur07@gmail.com>
2024-03-29 00:11:01 +0600

    Adding Day4's code and notes

---
+++ day4/README.md
@@ -0,0 +1,22 @@
+# Sneaky Extra Skill
+<br />
+
+## syntax 
+**print** ("\033[colour value m", "**TEXT**")
+
+## Example
+print ("\033[31m", "Warning", "\033[32m", "Safe")<br />
+here "Warning" will be printed in red colour and "Safe" will be printed in green colour.
+
+
+| Colour    | Value     |
+|-----------|-----------|
+| Default   | 0         |
+| Black     | 30        |
+| Red       | 31        |
+| Green     | 32        |
+| Yellow    | 33        |
+| Blue      | 34        |
+| Purple    | 35        |
+| Cyan      | 36        |
+| White     | 37        |
---
+++ day4/day4_project_and_sneaky_skill.py
@@ -0,0 +1,37 @@
+
+"""
+Build a custom story for custom user like this
+
+=== Your Adventure Simulator ===
+You'll be asked a bunch of questions then we'll make you up an amazing story with YOU as the
+
+name = input ("Your name: ")
+enemy = input ("Your worst enemy's name: ")
+superpower = input ("Your super power: ")
+
+Our story begins as our hero name approaches a foreboding castle...
+Suddenly a bolt of lighting striked the ground at the feet of, name
+Our final battle begins! shouts the evil, enemy, clearly missing the fact that, name, has the power of, superpower, which means they'll win quite easily
+
+"""
+
+
+
+###     Solution    ###
+
+print ("=== Your Adventure Simulator ===")
+print ("""You'll be asked a bunch of questions then we'll make you up an amazing story with YOU as the""")
+print()
+name = input ("Your name: ")
+enemy = input ("Your worst enemy's name: ")
+superpower = input ("Your super power: ")
+print ()
+print ("Our story begins as our hero name approaches a foreboding castle...")
+print ("Suddenly a bolt of lighting striked the ground at the feet of", name)
+print ("'Our final battle begins!' shouts the evil", enemy, "clearly missing the fact that", name, "has the power of", superpower, "which means they'll win quite easily")
+
+
+
+####    Sneaky Extra Skill 
+
+print ("Uh, oh, you've been given a", "\033[31m", "warning", "\033[0m","for being a bad, bad person.")  ## Check README file for explation.
